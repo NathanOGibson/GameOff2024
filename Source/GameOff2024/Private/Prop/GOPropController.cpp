1628f5f688da1b89a2477d49312bbfd55c0dd82a
Nate Gibson <00nathan.gibson00@gmail.com>
2024-11-08 15:18:45 +1300

    Prop controller setup.

--- Source/GameOff2024/Private/Prop/GOPropController.cpp
+++ Source/GameOff2024/Private/Prop/GOPropController.cpp
@@ -4,6 +4,7 @@
 #include "Prop/GOPropController.h"
 #include "Prop/GOProp.h"
 #include "Kismet/GameplayStatics.h"
+#include "Math/Vector.h"
 
 AGOPropController::AGOPropController()
 {
@@ -18,6 +19,10 @@ void AGOPropController::BeginPlay()
 	Super::BeginPlay();
 	GetPropReferences();
 	AssignChosenAndExtraProps();
+
+	ResetPropPositions();
+	SetSpawnPoints();
+	SetPropPositions();
 }
 
 void AGOPropController::GetPropReferences()
@@ -48,6 +53,98 @@ void AGOPropController::AssignChosenAndExtraProps()
 		if (!ExtraProps.Contains(Prop) && Prop != ChosenProp)
 		{
 			ExtraProps.Add(Prop);
+		}
+	}
+}
+
+void AGOPropController::ResetPropPositions()
+{
+	for (AGOProp* Prop : AllProps)
+	{
+		Prop->SetActorLocation(GetActorLocation());
+		Prop->SetActorRotation(FRotator(0.f, 0.f, 0.f));
+	}
+}
+
+void AGOPropController::SetSpawnPoints()
+{
+	TArray<USceneComponent*> ChildrenComponents;
+	GetComponents(ChildrenComponents);
+	ChildrenComponents.RemoveAt(0);
+
+	USceneComponent* CurrentChild = nullptr;
+
+	int32 RandomIndex = FMath::RandRange(0, ChildrenComponents.Num() - 1);
+	CurrentChild = ChildrenComponents[RandomIndex];
+	GEngine->AddOnScreenDebugMessage(-1, 100.0f, FColor::Green, FString::Printf(TEXT("Actor Name: %s"), *CurrentChild->GetName()));
+	ChildrenSpawnLocations.Add(CurrentChild);
+	ChildrenComponents.Remove(CurrentChild);
+
+	while (ChildrenSpawnLocations.Num() < 6 && CurrentChild != nullptr && ChildrenComponents.Num() > 0)
+	{
+		CurrentChild = GetFurthestSpawnPoint(CurrentChild, ChildrenComponents);
+		GEngine->AddOnScreenDebugMessage(-1, 100.0f, FColor::Green, FString::Printf(TEXT("Actor Name: %s"), *CurrentChild->GetName()));
+		ChildrenSpawnLocations.Add(CurrentChild);
+		ChildrenComponents.Remove(CurrentChild);
+	}
+}
+
+USceneComponent* AGOPropController::GetFurthestSpawnPoint(USceneComponent* StartPoint, TArray<USceneComponent*> ChildrenPoints)
+{
+	USceneComponent* FurthestSpawnPoint = nullptr;
+	float FurthestDistance = -1.f;
+	float SkipChance = 0.2f;
+
+	for (USceneComponent* SpawnPoint : ChildrenPoints)
+	{
+		// Random chance to skip the object
+		if (FMath::FRand() < SkipChance)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 100.0f, FColor::Red, FString::Printf(TEXT("SKIPPED")));
+
+			continue;  // Skip this iteration if the random chance is below the threshold
+		}
+
+		float Distance = FVector::Dist(StartPoint->GetComponentLocation(), SpawnPoint->GetComponentLocation());
+		if (Distance > FurthestDistance)
+		{
+			FurthestSpawnPoint = SpawnPoint;
+			FurthestDistance = Distance;
+		}
+	}
+
+	return FurthestSpawnPoint;
+}
+
+void AGOPropController::SetPropPositions()
+{
+	TArray<TObjectPtr<AGOProp>> PlacementProps;
+	PlacementProps.Add(ExtraProps[0]);
+	PlacementProps.Add(ExtraProps[1]);
+	while (PlacementProps.Num() < 6)
+	{
+		AGOProp* Prop = PickProp();
+		if (!ExtraProps.Contains(Prop) && Prop != ChosenProp && !PlacementProps.Contains(Prop))
+		{
+			PlacementProps.Add(Prop);
+		}
+	}
+
+	for (int32 i = 0; i < ChildrenSpawnLocations.Num(); i++)
+	{
+		USceneComponent* Child = ChildrenSpawnLocations[i];
+
+		if (PlacementProps.Num() > 0)
+		{
+			int32 RandomIndex = FMath::RandRange(0, PlacementProps.Num() - 1);
+			TObjectPtr<AGOProp> Prop = PlacementProps[RandomIndex];
+
+			//GEngine->AddOnScreenDebugMessage(-1, 100.0f, FColor::Green, FString::Printf(TEXT("Actor Name: %s"), *Prop.GetName()));
+
+			Prop->SetActorLocation(Child->GetComponentLocation());
+			Prop->SetActorRotation(Child->GetComponentRotation());
+
+			PlacementProps.RemoveAt(RandomIndex);
 		}
 	}
 }
--- Source/GameOff2024/Public/Prop/GOPropController.h
+++ Source/GameOff2024/Public/Prop/GOPropController.h
@@ -31,10 +31,25 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Prop")
 	TArray<TObjectPtr<AGOProp>> ExtraProps;
 
+	UPROPERTY(VisibleAnywhere, Category = "Prop")
+	TArray<USceneComponent*> ChildrenSpawnLocations;
+
+	/** Selecting Props*/
 	void GetPropReferences();
 
 	TObjectPtr<AGOProp> PickProp();
+
 	void AssignChosenAndExtraProps();
+
+	/** Positioning Props*/
+	void ResetPropPositions();
+
+	void SetSpawnPoints();
+
+	USceneComponent* GetFurthestSpawnPoint(USceneComponent* StartPoint, TArray<USceneComponent*> ChildrenPoints);
+
+	void SetPropPositions();
+
 
 public:
 	virtual void Tick(float DeltaTime) override;
